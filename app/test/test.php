e66d346695f7ce57dd26179176223c22a0e585aa
LuuKasBR <lsdo2011@gmail.com>
2020-02-06 20:09:07 -0300

    First commit

---
+++ app/test/test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Teste</title>
+</head>
+<body>
+    <h1>Working</h1>
+    <hr>
+    <?php
+        echo '<pre>';
+        var_dump($_SERVER);
+        echo '</pre>';
+    ?>
+</body>
+</html>
